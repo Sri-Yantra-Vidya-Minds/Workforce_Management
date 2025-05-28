bbbacae700bcf3911722aa29a71a25ddfd1f6649
sampathsriyv <sampath@syvminds.com>
2025-05-28 22:54:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {
+      //  Add your theme customizations here (colors, fonts, etc.)
+    },
+  },
+  plugins: [
+    //  Add any Tailwind plugins here
+  ],
+};
